85089216d206d37b17016220ef887f3f35130d02
Akshit Mittal <99958806+Akshitm970@users.noreply.github.com>
2025-02-16 12:18:27 +0530

    Time: 7 ms (6.8%), Space: 44.9 MB (26.35%) - LeetHub

---
+++ 1651-shuffle-string/1651-shuffle-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] arr = new char[indices.length];
+        for(int i = 0;i<indices.length;i++){
+            int pos = indices[i];
+            arr[pos] = s.charAt(i);
+        }
+        String st = "";
+        for(int i = 0;i<arr.length;i++){
+            st = st+arr[i];
+        }
+        return st;
+    }
+}
